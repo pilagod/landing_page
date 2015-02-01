13448e59e648dd4eab0a2b5895f96ab66597eb9b
pilagod <r03725029@ntu.edu.tw>
2015-02-01 19:30:25 +0800

    node server

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by pilagod on 2/1/15.
+ */
+
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+server.listen(3000, function(){
+    console.log("listening on *:3000");
+});
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res){
+    res.sendFile('index.html', {"root": __dirname});
+});
