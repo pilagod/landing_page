2ea576c6f15a04e148a0a1eb2d69bac503a565f8
pilagod <r03725029@ntu.edu.tw>
2015-04-21 21:20:46 +0800

    delete module cached

--- public/build/.module-cache/70b784a8b1e0cf6525743ff0681474fc54ede258.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by pilagod on 4/21/15.
- */
-$(document).ready(function(){
-    console.log("ready");
-    $(window).on('scroll', function(){
-        console.log($(window).scrollTop());
-    })
-});
--- public/build/.module-cache/fd3445d13da65f1203c27f1cd52f97214ac11352.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by pilagod on 4/21/15.
- */
-$(document).ready(function(){
-    console.log("ready");
-    console.log($(window).scrollTop());
-});
